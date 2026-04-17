c40b5def61c9f136aaf98d5e5b577411ec35d99a
dtrbinu <dtrbinu@gmail.com>
2026-04-17 10:08:30 +0700

    feat: add Task model and users-tasks relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
